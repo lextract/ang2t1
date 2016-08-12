a2884b1e2963b90d5f8af61689ca54fc8206ab22
john <johnantonior@gmail.com>
2016-08-12 18:51:10 -0500

    se adicionan typings de node y express

--- server.ts
+++ server.ts
@@ -1,8 +1,8 @@
 import * as express from "express";
 import * as fs from "fs";
 import * as path from "path";
-import {BaseApp} from "./servercommon/BaseApp";
-import * as AppsLoader from "./servercommon/AppsLoader";
+// import {BaseApp} from "./servercommon/BaseApp";
+// import * as AppsLoader from "./servercommon/AppsLoader";
 //import * as Logog from './node_modules/morgan/index.js';
 //let morgan = require("morgan");
 
@@ -11,7 +11,7 @@ let eapp = express();
 //eapp.use(morgan('dev'));
 eapp.use("/node_modules", express.static(path.join(__dirname,"node_modules")));
 eapp.use("/clientcommon", express.static(path.join(__dirname,"clientcommon")));
-let appsLoaded = AppsLoader.processAppDirs(__dirname,eapp);
+//let appsLoaded = AppsLoader.processAppDirs(__dirname,eapp);
 
 var server = eapp.listen(5000, function() {
     console.log("servidor INICIADO!!!");
